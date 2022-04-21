7c8d9c239baf2215b82c8756bc3c25ed3eb63a8b
Denis Shahbazyan <den.shahbazyan@yandex.ru>
2022-04-21 13:40:23 +0300

    Обновил пермишен

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -10,13 +10,13 @@ class IsAuthorOrIsAdminOrReadOnly(BasePermission):
         return bool(
             request.method in SAFE_METHODS
             or request.user.is_authenticated
-            or request.user.is_staff
         )
 
     def has_object_permission(self, request, view, obj):
         return bool(
             request.method in SAFE_METHODS
             or obj.author == request.user
+            or request.user.is_staff
         )
 
 
