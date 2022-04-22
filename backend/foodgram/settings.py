55529f8babf98a343bc3080d09b5d3455bb93ea6
Denis Shahbazyan <den.shahbazyan@yandex.ru>
2022-04-23 02:45:20 +0300

    добавил хосты в настройки джанго

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['51.250.21.130', '127.0.0.1', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://127.0.0.1']
 
